1f207233c5633bc96eaa2fd5accfef3ae5414edd
gson Liang <yuanyu90221@gmail.com>
2021-12-11 20:13:24 +0800

    another nested route

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,12 +1,10 @@
-import { Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Welcome = () => {
   return (
   <section>
     <h1>The Welcome Page</h1>
-    <Routes>
-      <Route path="/new-user" element={<p>Welcome, new user!</p>} />
-    </Routes>
+    <Link to="new-user">New User</Link>
   </section>);
 };
 

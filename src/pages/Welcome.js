dea874e081bf293db66fb1023836f502f533b85e
gson Liang <yuanyu90221@gmail.com>
2021-12-11 20:15:10 +0800

    outlet for nested content

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,10 +1,11 @@
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 const Welcome = () => {
   return (
   <section>
     <h1>The Welcome Page</h1>
     <Link to="new-user">New User</Link>
+    <Outlet />
   </section>);
 };
 
